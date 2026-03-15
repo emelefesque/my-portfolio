f1f362ad815babb873fa455db67f0b67a21dd579
Megan feltes <meganfeltes@gmail.com>
2026-03-15 11:49:02 -0500

    Add MDX extensions to next.config.mjs
    fixing errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
-mport createMDX from "@next/mdx";
-const withMDX = createMDX();
+import createMDX from "@next/mdx";
+const withMDX = createMDX({
+  extensions: [".mdx", ".md"],
+});
 export default withMDX({
-  pageExtensions: ["js", "jsx", "md", "mdx"]
+  pageExtensions: ["js", "jsx", "md", "mdx"],
 });
